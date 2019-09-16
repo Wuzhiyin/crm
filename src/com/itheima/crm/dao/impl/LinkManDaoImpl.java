e8254e6e13abd2e488beaa25aa711e121a407b36
Wilson <979446886@qq.com>
2019-09-16 10:00:20 +0800

    第一次修改

---
+++ src/com/itheima/crm/dao/impl/LinkManDaoImpl.java
@@ -0,0 +1,7 @@
+package com.itheima.crm.dao.impl;
+
+import com.itheima.crm.dao.LinkManDao;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+public class LinkManDaoImpl extends HibernateDaoSupport implements LinkManDao {
+}
