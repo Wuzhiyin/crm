f4eff4fd0e71455a7b1cf9a64fdfed7a7eed06f9
Wilson <979446886@qq.com>
2019-09-17 14:11:21 +0800

    级联删除操作

---
+++ src/com/itheima/crm/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,26 @@
+package com.itheima.crm.dao.impl;
+
+import com.itheima.crm.dao.BaseDao;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+/**
+ * 通用的DAO的实现类
+ * @param <T>
+ */
+public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T> {
+
+    @Override
+    public void save(T t) {
+        this.getHibernateTemplate().save(t);
+    }
+
+    @Override
+    public void update(T t) {
+        this.getHibernateTemplate().update(t);
+    }
+
+    @Override
+    public void delete(T t) {
+        this.getHibernateTemplate().delete(t);
+    }
+}
